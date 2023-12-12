2b592752b8530d452f2c92bf5842a788d994a9a1
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-12 03:34:24 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,29 @@
+$(document).ready(function(){
+  disableOptions();
+  $("#productId").on("change", function(){
+    $("#fromLocation option").not(":first").remove();
+    if ($("#productId").val()) {
+      ajaxcallCall("get-from-locations");
+      enableOptions();
+    } else {
+      disableOptions();
+  }
+    retuern false;
+});
 
+  $("#submitLocation").on("click", function(e){
+    e.preventDefault();
+    $.ajax({
+      data: {
+        loaction: $("#location_name").val(),
+      },
+      type: "POST",
+      url: "/dub-locations/"
+    }).done(function (data) {
+      if (data.output) {
+        $("#location_form").submit();
+      } else {
+        alert("This Name is already used, please choose other one.");
+      }
+    });
+  });
