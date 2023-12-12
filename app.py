54a0f949456dae1e60a1eeef7bb64dd64288b51f
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-12 20:21:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask. render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from collections import defaultdict
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite://inventory.db'
-db = SQLAchemy(app)
+db = SQLAlchemy(app)
 
 
 class Product(db.Model):
