58d3d3303b96e004a47f532bbd83b76a05ccac70
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-12 21:49:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://inventory.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
 db = SQLAlchemy(app)
 
 
