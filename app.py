4c5b8a83dafa3ab4060c968af83c5847765f191b
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-12 21:51:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -26,21 +26,20 @@ class Location(db.Model):
         return '<Location %r>' % self.location_id
 
 class ProductMovement(db.Model):
+    __tablename__ = 'productmovements'
+    movement_id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.String(200), db.ForeignKey('products.product_id'))
+    qty = db.Column(db.Integer)
+    from_location = db.Column(db.String(200), db.ForeignKey('locations.location_id'))
+    to_location = db.Column(db.String(200), db.ForeignKey('locations.location_id'))
+    movement_time = db.Column(db.DateTime, default=datetime.utcnow)
 
-__tablename__ = 'productmovements'
-movement_id   = db.Column(db.Integer, primary_key=True)
-product_id    = db.Column(db.String(200), db.Foreignkey('products.product_id'))
-qty           = db.Column(db.Integer)
-from_location = db.Column(db.String(200), db.ForeignKey('locations.location_id'))
-to_location   = db.Column(db.String(200), db.ForeignKey('locations.location_id'))
-movement_time = db.Column(db.DateTime, default=datetime.utcnow)
+    product = db.relationship('Product', foreign_keys=product_id)
+    fromLoc = db.relationship('Location', foreign_keys=from_location)
+    toLoc = db.relationship('Location', foreign_keys=to_location)
 
-product       = db.relationship('Product', foreign_keys=product_id)
-fromLoc       = db.relationship('Location', foreign_keys=from_location)
-toloc         = db.relationship('Location', foreign_keys=to_location)
-
-def __repr__(self):
-  return '<ProductMovement %r>' % self.movement_id
+    def __repr__(self):
+        return '<ProductMovement %r>' % self.movement_id
 
 @app.route('/', methods=["POST", "GET"])
 def index():
