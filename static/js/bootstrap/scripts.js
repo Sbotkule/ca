c9f9b1a7e28d484ea27338de207ce122070523f5
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-13 22:55:52 +0000

    Update scripts.js

--- static/js/bootstrap/scripts.js
+++ static/js/bootstrap/scripts.js
@@ -1,1 +1,23 @@
+/*!
+    *Start Bootstrap - SB Admin v6.0.1 (https://startbootstrap.com/templates/sb-admin)*
+    *Copyright 2013-2020 start Bootstarp*
+    *Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+     (funstion($) {
+       "use strict";
 
+       // Add active state to sidebar nav links
+       var path = window.locations.href; // because the 'href' property of the DOM element is the absolute path
+           $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+               if (this.href === path) {
+                 $(this).addClass("active");
+                }
+            });
+
+       / Toggle the sode navigation
+       $("#sidebarToggle").on("click", funstion(e) {
+           e.preventDefault();
+           $("body").toggleClass("sb-sidenav-toggled");
+       });
+})(jQuery);
+    
