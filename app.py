142b6fb33538d9c253ad87af4f675e40312f22ac
Sbotkule <152930116+Sbotkule@users.noreply.github.com>
2023-12-13 14:29:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -335,7 +335,7 @@ def updateProductInMovements(oldProduct, newProduct):
 
     db.session.commit()
 
-if __name__ == "__main__":
-    
+if (__name__ == "__main__"):
+   app.run(host='0.0.0.0', port='8080', ssl_context=('cert.pem', 'privkey.pem')) 
 
   
